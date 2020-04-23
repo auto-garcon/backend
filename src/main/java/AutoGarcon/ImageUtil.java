ecf21c677f1b754b0adc57d6b67c8912e2003368
Tyler Beverley <tylerbeverley@gmail.com>
2020-04-23 13:23:16 -0500

    add imageUtil documentation

--- src/main/java/AutoGarcon/ImageUtil.java
+++ src/main/java/AutoGarcon/ImageUtil.java
@@ -30,6 +30,7 @@ public class ImageUtil {
      *
      * @param menuID - the menuID of the image to be saved. 
      * @param menuItemID - the menuItemID of the image to be saved. 
+     * @param is - an InputStream of image bytes. 
      * @return True if saved correctly, false otherwise. 
      */
     public static boolean saveImage( int menuID, int menuItemID, InputStream is ){
@@ -46,10 +47,15 @@ public class ImageUtil {
                     "Exception: %s\n", ioe.toString() );
             return false; 
         }
-
-
     }
 
+    /**
+     * saveImage: Saves an image file to the server given a byte array. 
+     * @param menuID - the menuID of the image to be saved. 
+     * @param menuItemID - the menuItemID of the image to be saved. 
+     * @param bytes - the bytes of the image to be saved. 
+     * @return File - The image file that was saved to the filesystem.
+     */
     public static File saveImage( int menuID, int menuItemID, byte[] bytes ){
 
         String path = String.format( basePath + "%d/%d", menuID, menuItemID );
@@ -67,6 +73,13 @@ public class ImageUtil {
         return image; 
     }
 
+    /**
+     * getImage: Gets a specifed image from the filesystem.
+     * @param menuID - menuID of the image to get. 
+     * @param menuItemID - itemID of the image to get. 
+     *
+     * @return File - The image file requested. 
+     */
     public static File getImage( int menuID, int menuItemID ) {
 
         String path = String.format( basePath + "%d/%d", menuID, menuItemID );
@@ -80,30 +93,56 @@ public class ImageUtil {
         return image; 
     }
 
+    /**
+     * getImageURL: gets the URL where you can download the image. 
+     * @param menuID - the menuID of the image you want a URL for. 
+     * @param menuItemID - the itemID of the image that you want to a URL for. 
+     *
+     * @return - The autogarcon url for retreiving the specifed image from the API. 
+     */
     public static String getImageURL( int menuID, int menuItemID ){
         return String.format( "https://autogarcon.live/api/images/"
                 + "%d/%d", menuID, menuItemID 
         );
     }
 
+
+    /**
+     * createMenuFolder: create a folder for a new menu to store images in. 
+     * @param menuID - menuID of the images that will be stored in the folder. 
+     */
     public static void createMenuFolder( int menuID ){
         File dir = new File( basePath + Integer.toString( menuID ) );
         boolean result = dir.mkdir(); 
     }
 
+    /**
+     * deserialize: get the image bytes from a json element. 
+     * @param json - the json element containing the image data.
+     * @return byte[] - the image data. 
+     */
     public static byte[] deserialize( JsonElement json ) {
         Base64.Decoder decoder = Base64.getDecoder(); 
         return decoder.decode(json.getAsString() );
     }
      
+    /**
+     * deserialize: get the image bytes from a string of bytes.  
+     * @param bytes - the string to deserialize into bytes.    
+     */
     public static byte[] deserialize( String bytes ){
         Base64.Decoder decoder = Base64.getDecoder(); 
         return decoder.decode( bytes );
     }
 
+    /**
+     * serialize: serialize the image bytes into a Json element.
+     * @param src - Image bytes to serialize into a json element.
+     *
+     * @return JsonElement - the resulting JSON containing the image data.  
+     */
     public static JsonElement serialize(byte[] src ){
         Base64.Encoder encoder = Base64.getEncoder(); 
         return new JsonPrimitive(encoder.encodeToString(src));
     }
-
 }
